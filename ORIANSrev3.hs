d567d9763e92bc1a83d8074bbbabfccd973f247d
Eli Orians <eli.orians@gmail.com>
2022-09-06 13:18:28 -0400

    initial for rev3

---
+++ ORIANSrev3.hs
@@ -0,0 +1,42 @@
+--Bubble Sorting--
+
+--1
+--bubble :: Ord => [a] -> [a]
+--bubble
+
+--2
+--bubbleSort :: Ord a=> [a] -> [a]
+--bubbleSort
+
+--Searching & Replacing Substrings--
+
+--1
+--findString :: String -> String -> String
+--findString
+
+--2
+--genSubStrings :: String -> [String]
+--genSubStrings
+
+--3
+--replacePrefix :: (String, String) -> String -> String
+--replacePrefix
+
+--4
+--replaceString :: (String, String) -> String -> String
+--replaceString
+
+
+--A simple cypher--
+
+--1
+--lookUp :: Char -> [(Char, Char)] -> Char
+--lookUp
+
+--2
+--encode :: [(Char, Char)] -> String -> String
+--encode
+
+--3
+--makeTable :: String -> String -> [(Char, Char)]
+--makeTable 
