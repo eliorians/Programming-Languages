e9a4b75886a9b1fc1373aa2801d39450ee3b7fac
Eli Orians <eli.orians@gmail.com>
2022-09-15 16:05:47 -0400

    more work

--- ORIANSrev4.hs
+++ ORIANSrev4.hs
@@ -1,33 +1,48 @@
+
+
 --currying and uncurrying--
 
---1 
-mapPair :: (a -> b-> c) -> [(a,b)] -> [c]
-mapPair f [] = []
-mapPair f ((x,y) : ps) = f x y : mapPair f ps 
+--1 done
+mapPair :: (a -> b -> c) -> [(a,b)] -> [c]
+mapPair = map . uncurry
 
---2
-mapPair' :: (a -> b-> c) -> [(b,a)] -> [c]
-mapPair' f [] = []
-mapPair' f ((x,y) : ps) = f y x : mapPair' f ps
+--2 
+--mapPair' :: (a -> b-> c) -> [(b,a)] -> [c]
+--mapPair' = map . curry
 
 --zipWith--
 
---1
+--1 done
+diff :: [Integer] -> [Integer] -> [Integer]
+diff = zipWith (-)
 
-
---2
+--2 
+--splice :: [String] -> [String] -> [String]
+--splice = 
 
 --map--
 
---1
---2
+--1 done
+sqLens :: [String] -> [Integer]
+sqLens = map ((^2) . fromIntegral . length)
+
+--2 done
+bang :: [String] -> [String]
+bang = map (++ "!")
+
 
 --filter--
 
---1
---2
+--1 done
+digitsOnly :: [Integer] -> [Integer]
+digitsOnly = filter (\x -> (x > 0 ) && (x < 10))
+
+--2 done
+removeXs :: [String] -> [String]
+removeXs = filter (notElem 'X')
 
 --folds--
+
 --1
 --2
 --3
